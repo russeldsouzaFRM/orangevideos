4cfdb9144bb91704f26118aa74c0f1b66de7aa3f
RusselDsouza029 <russeldsouza456@gmail.com>
2023-12-14 18:59:30 +0530

    made some changes

--- src/components/careers/ApplyForm/ApplyForm.jsx
+++ src/components/careers/ApplyForm/ApplyForm.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
-import EmailForm from "./EmailForm";
 import axios from "axios";
 
 const ApplyForm = () => {
